f2ce93958d628d1968ed15e7d6dfa8d1218f5b3c
Michio Ogawa <michioga@bekkoame.ne.jp>
2019-06-11 16:25:27 +0900

    added link to www.frontistr.com

--- markdown_files/pdf_scripts/mdtopdf_theory.sh
+++ markdown_files/pdf_scripts/mdtopdf_theory.sh
@@ -3,30 +3,21 @@
 CWD=`pwd`
 FILES="index.md theory_01.md theory_02.md theory_03.md theory_04.md theory_05.md theory_06.md theory_07.md"
 
-cd ../ja/docs/theory
-cat $FILES > theory.md
-pandoc -f markdown+link_attributes+tex_math_dollars \
-	theory.md \
-	-t html5 \
-	-o ../../../pdf/theory_ja.pdf \
-	-s --mathjax --pdf-engine=wkhtmltopdf \
-	--metadata pagetitle="FrontISTR 理論マニュアル" \
-	--dpi=300 --toc --number-sections --css ../css/extra.css \
-	-V papersize:a4 -V margin-left:18.7mm -V margin-right:18.7mm \
-        -V margin-top:18.7mm -V margin-bottom:18.7mm
-rm -rf theory.md *.html
+for i in en ja
+do
+  cd ../$i/docs/theory
+  cat $FILES > tmp.md
+  pandoc -f markdown+link_attributes+tex_math_dollars \
+    tmp.md \
+    -t html5 \
+    -o ../../../pdf/theory_$i.pdf \
+    -s --mathjax --pdf-engine=wkhtmltopdf \
+    --metadata pagetitle="FrontISTR Theory manual" \
+    --toc --number-sections --css ../css/extra.css \
+    -V papersize:a4 -V margin-left:18.7mm -V margin-right:18.7mm \
+    -V margin-top:18.7mm -V margin-bottom:18.7mm
+  rm -rf tmp.md *.html
+  cd $CWD
+done
 
-cd $CWD
-cd ../en/docs/theory
-cat $FILES > theory.md
-pandoc -f markdown+link_attributes+tex_math_dollars \
-	theory.md \
-	-t html5 \
-	-o ../../../pdf/theory_en.pdf \
-	-s --mathjax --pdf-engine=wkhtmltopdf \
-	--metadata pagetitle="FrontISTR Theory manual" \
-	--dpi=300 --toc --number-sections --css ../css/extra.css \
-	-V papersize:a4 -V margin-left:18.7mm -V margin-right:18.7mm \
-        -V margin-top:18.7mm -V margin-bottom:18.7mm
-rm -rf theory.md *.html
 
